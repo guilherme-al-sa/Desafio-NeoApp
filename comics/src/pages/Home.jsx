4aae447551efbc300aa71d2d391acec2567472c9
guilherme-al-sa <guilhermeswsw@gmail.com>
2025-08-09 18:36:20 -0300

    att home

--- comics/src/pages/Home.jsx
+++ comics/src/pages/Home.jsx
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from "react";
 import { fetchComics } from "../api/marvelClient";
 import ComicCard from "../components/ComicCard";
 import Pagination from "../components/Pagination";
+import {useNavigate} from 'react-router-dom';
+import { useCart } from "../context/CartProvider";
 
 export default function Home() {
  const LIMIT = 12;
  const [page, setPage] = useState(0);
  const [data, setData] = useState({ results: [], total: 0, offset: 0, limit: LIMIT });
  const [loading, setLoading] = useState(false);
+ const navigate = useNavigate();
+ const {addItem} = useCart();
 
  useEffect(() => {
   setLoading(true);
@@ -23,13 +27,19 @@ export default function Home() {
 
  return (
   <main className="container">
+    <header className="topbar">
    <h1>Loja de quadrinhos</h1>
+   </header>
    {loading ? <p>Carregando...</p> : (
-    <div className="grid">
-     {data.results.map(c => <ComicCard key={c.id} comic={c} onOpen={() => {/* navegar para detalhe */ }} onAdd={() => {/* adicionar ao carrinho */ }} />)}
-    </div>
-   )}
-   <Pagination page={page} totalPages={totalPages} onChange={p =>setPage(p)} />
-  </main>
+      <>
+          <div className="grid">
+            {data.results.map(c => (
+              <ComicCard key={c.id} comic={c} onOpen={(id)=>navigate(`/comics/${id}`)} onAdd={(item)=>addItem(item)} />
+            ))}
+          </div>
+          <Pagination page={page} totalPages={totalPages} onChange={p => setPage(Math.max(0, p))} />
+        </>
+      )}
+    </main>
  );
 }
