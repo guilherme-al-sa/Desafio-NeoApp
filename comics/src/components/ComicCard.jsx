be1d7f6c5d4d2b5da8e625739b8ffebfd9c0916f
guilherme-al-sa <guilhermeswsw@gmail.com>
2025-08-09 18:25:04 -0300

    atualizando card

--- comics/src/components/ComicCard.jsx
+++ comics/src/components/ComicCard.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 export default function ComicCard({comic,onOpen,onAdd}){
  const thumb = `${comic.thumbnail.path}/portrait_uncanny.${comic.thumbnail.extension}`;
- const price =  comic.prices?.[0]?.price ?? 0;
+ const price =  (comic.price && comic.prices[0] && comic.prices[0].price) || 0;
  return (
   <article className="comic-card">
    <img src={thumb} alt={comic.title} />
-   <h3>{comic.title}</h3>
-   <p>{price ? `R${price.toFixed(2)}` : "Grátis"}</p>
+   <h3 className="comic-title">{comic.title}</h3>
+   <p className="comic-price">{price ? `R${price.toFixed(2)}` : "Grátis"}</p>
    <div className="comic-actions">
-    <button onClick={() =>onOpen(ComicCard.id)}>Ver</button>
-    <button onClick={()=>onAdd({id: comic.id, title: comic.title, price,thumb})}>Adicionar</button>
+    <button className="btn" onClick={() =>onOpen(comic.id)}>Ver</button>
+    <button className="btn primary" onClick={()=>onAdd({id: comic.id, title: comic.title, price,thumb})}>Adicionar</button>
    </div>
   </article>
- )
+ );
 }
