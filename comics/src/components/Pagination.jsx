b1ac655b09bac31309c176593440e40beeed9515
guilherme-al-sa <guilhermeswsw@gmail.com>
2025-08-09 18:27:35 -0300

    atualizando pagination

--- comics/src/components/Pagination.jsx
+++ comics/src/components/Pagination.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Pagination({page, totalPages, onChange}){
  return(
-  <div className="pgination">
-   <button onClick={() =>onChange(page1)} disabled={page<=0}>Anterior</button>
-   <span>Página {page+1} de {totalPages}</span>
+  <div className="pagination">
+   <button onClick={() =>onChange(page - 1)} disabled={page <=0}>Anterior</button>
+   <span>Página {page+1} de {totalPages || 1}</span>
    <button onClick={()=> onChange(page+1)} disabled={page+1>=totalPages}>Proxima</button>
   </div>
  );
